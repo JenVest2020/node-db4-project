754aa902ca400b705b69dfb2ea919a5ba9acb1fb
JenVest2020 <jenvest2019@gmail.com>
2020-09-25 17:12:09 -0500

    getInstructions issue resolved/shoppingList issue working

--- models/db_model.js
+++ models/db_model.js
@@ -1,4 +1,4 @@
-const { select } = require('../data/db-config');
+const { select, orderBy } = require('../data/db-config');
 const db = require('../data/db-config');
 
 
@@ -9,15 +9,16 @@ function getRecipes() {
 function getShoppingList(recipe_id) {
     return db('Recipes as r')
         .join('Ingredients as i', 'i.id', ' ri.ingredients_id')
-        .join('RecipesAndIngredients as ri', 'ri.id', 'ri.recipe_id')
-        .where('r.id', recipe_id)
-        .select('i.name', 'ri.quantity');
+        .join('RecipesAndIngredients as ri')
+        .where('ri.recipe_id', recipe_id)
+        .orderBy('r.id')
+        .select('i.id as IngredientId', 'i.name', 'ri.quantity', 'r.name as RecipeName');
 }
 
 function getInstructions(recipe_id) {
     return db('Recipes as r')
         .join('Instructions as i', 'r.id', 'i.recipe_id')
-        .where('r.id', 'i.recipe_id')
+        .where('i.recipe_id', recipe_id)
         .select('i.step', 'i.instruction', 'r.name as RecipeName')
         .orderBy('r.id')
 }
