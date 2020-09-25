4168061404c7f9b5d36624f734a7121152e68290
JenVest2020 <jenvest2019@gmail.com>
2020-09-24 21:59:47 -0500

    migration tables completed

---
+++ data/migrations/20200924212224_create-tables.js
@@ -0,0 +1,54 @@
+
+exports.up = function (knex) {
+    return knex.schema.createTable('Ingredients', tbl => {
+        tbl.increments();
+        tbl.string('ingredient_name', 128).notNullable();
+        tbl.string('measurement', 128).notNullable();
+        tbl.varchar('ingredient_image');
+        tbl.integer('ingredient_qnty').notNullable();
+    })
+        .createTable('Instructions', tbl => {
+            tbl.increments();
+            tbl.integer('step_number').notNullable();
+            tbl.string('step_name', 128);
+            tbl.string('step_instructions').notNullable();
+        })
+        .createTable('Recipes', tbl => {
+            tbl.increments();
+            tbl.string('recipe_name', 128).notNullable().unique();
+            tbl.float('ingredient_qnty')
+                .unsigned()
+                .notNullable()
+                .references('Ingredients.ingredient_qnty')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE');
+            tbl.integer('instructions_id')
+                .unsigned()
+                .notNullable()
+                .references('Instructions.id')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE');
+        })
+        .createTable('RecipesAndIngredients', tbl => {
+            tbl.integer('recipe_id')
+                .unsigned()
+                .notNullable()
+                .references('Recipes.id')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE');
+            tbl.integer('ingredient_id')
+                .unsigned()
+                .notNullable()
+                .references('Ingredients.id')
+                .onDelete('CASCADE')
+                .onUpdate('CASCADE');
+            tbl.primary(['recipe_id', 'ingredient_id'])
+        })
+};
+
+exports.down = function (knex) {
+    return knex.schema.dropTableIfExists('RecipesAndIngredients')
+        .dropTableIfExists('Recipes')
+        .dropTableIfExists('Instructions')
+        .dropTableIfExists('Ingredients');
+};
